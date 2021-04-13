ee002b44df3bb9183dc3218f4ce76a546de1826b
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-04-13 22:06:39 +0700

    Create MainViewModelTest.kt

--- MyUnitTesting/app/src/test/java/com/erikriosetiawan/myunittesting/MainViewModelTest.kt
+++ MyUnitTesting/app/src/test/java/com/erikriosetiawan/myunittesting/MainViewModelTest.kt
@@ -10,10 +10,6 @@ class MainViewModelTest {
     private lateinit var mainViewModel: MainViewModel
     private lateinit var cuboidModel: CuboidModel
 
-    private val dummyLength = 12.0
-    private val dummyWidth = 7.0
-    private val dummyHeight = 6.0
-
     private val dummyVolume = 504.0
     private val dummyCircumference = 100.0
     private val dummySurfaceArea = 396.0
