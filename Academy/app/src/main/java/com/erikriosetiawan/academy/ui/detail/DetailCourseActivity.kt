93c6fa6e2621117cb638cde9ead58c8090c29d96
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-04-14 19:46:22 +0700

    Integrated the view binding

--- Academy/app/src/main/java/com/erikriosetiawan/academy/ui/detail/DetailCourseActivity.kt
+++ Academy/app/src/main/java/com/erikriosetiawan/academy/ui/detail/DetailCourseActivity.kt
@@ -2,19 +2,22 @@ package com.erikriosetiawan.academy.ui.detail
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.erikriosetiawan.academy.R
+import com.erikriosetiawan.academy.databinding.ActivityDetailCourseBinding
+import com.erikriosetiawan.academy.databinding.ContentDetailCourseBinding
 
 class DetailCourseActivity : AppCompatActivity() {
 
-    companion object {
-        const val EXTRA_COURSE = "extra_course"
-    }
+    private lateinit var detailContentBinding: ContentDetailCourseBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail_course)
-        setSupportActionBar(findViewById(R.id.toolbar))
 
+        val activityDetailCourseBinding = ActivityDetailCourseBinding.inflate(layoutInflater)
+        detailContentBinding = activityDetailCourseBinding.detailContent
+
+        setContentView(activityDetailCourseBinding.root)
+
+        setSupportActionBar(activityDetailCourseBinding.toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 }
