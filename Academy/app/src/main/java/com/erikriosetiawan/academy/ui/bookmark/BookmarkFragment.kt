6aab1a88d3cecc00a0f75309ea01759af078c822
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-04-14 21:19:12 +0700

    Integrated the RecyclerView

--- Academy/app/src/main/java/com/erikriosetiawan/academy/ui/bookmark/BookmarkFragment.kt
+++ Academy/app/src/main/java/com/erikriosetiawan/academy/ui/bookmark/BookmarkFragment.kt
@@ -1,60 +1,54 @@
 package com.erikriosetiawan.academy.ui.bookmark
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ShareCompat
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.erikriosetiawan.academy.R
+import com.erikriosetiawan.academy.data.CourseEntity
+import com.erikriosetiawan.academy.databinding.FragmentBookmarkBinding
+import com.erikriosetiawan.academy.utils.DataDummy
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class BookmarkFragment : Fragment(), BookmarkFragmentCallback {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [BookmarkFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class BookmarkFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var fragmentBookmarkBinding: FragmentBookmarkBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bookmark, container, false)
+    ): View {
+        fragmentBookmarkBinding = FragmentBookmarkBinding.inflate(inflater, container, false)
+        return fragmentBookmarkBinding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment BookmarkFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            BookmarkFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        activity?.let {
+            val courses = DataDummy.generateDummyCourses()
+            val adapter = BookmarkAdapter(this)
+            adapter.setCourses(courses)
+
+            with(fragmentBookmarkBinding.rvBookmark) {
+                layoutManager = LinearLayoutManager(context)
+                setHasFixedSize(true)
+                this.adapter = adapter
             }
+        }
+    }
+
+    override fun onShareClick(course: CourseEntity) {
+        activity?.let {
+            val mimeType = "text/plain"
+            ShareCompat.IntentBuilder
+                .from(requireActivity())
+                .setType(mimeType)
+                .setChooserTitle("Bagikan aplikasi ini sekarang.")
+                .setText(resources.getString(R.string.share_text, course.title))
+                .startChooser()
+        }
     }
 }
