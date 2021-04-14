7750e858e17b88e860f962ce3660dc77b5cb8b0a
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-04-14 18:37:31 +0700

    Display the back arrow

--- Academy/app/src/main/java/com/erikriosetiawan/academy/DetailCourseActivity.kt
+++ Academy/app/src/main/java/com/erikriosetiawan/academy/DetailCourseActivity.kt
@@ -1,20 +1,19 @@
 package com.erikriosetiawan.academy
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 
 class DetailCourseActivity : AppCompatActivity() {
+
+    companion object {
+        const val EXTRA_COURSE = "extra_course"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_course)
         setSupportActionBar(findViewById(R.id.toolbar))
 
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 }
