a2d6de4fe3f0c479d0a227fa1ca5225454f22afc
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-04-14 20:55:31 +0700

    Create AcademyAdapter.kt

---
+++ Academy/app/src/main/java/com/erikriosetiawan/academy/ui/academy/AcademyAdapter.kt
@@ -0,0 +1,61 @@
+package com.erikriosetiawan.academy.ui.academy
+
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.erikriosetiawan.academy.R
+import com.erikriosetiawan.academy.data.CourseEntity
+import com.erikriosetiawan.academy.databinding.ItemsAcademyBinding
+import com.erikriosetiawan.academy.ui.detail.DetailCourseActivity
+
+class AcademyAdapter : RecyclerView.Adapter<AcademyAdapter.CourseViewHolder>() {
+
+    private var listCourses = ArrayList<CourseEntity>()
+
+    fun setCourses(courses: List<CourseEntity>?) {
+        courses?.let {
+            this.listCourses.clear()
+            this.listCourses.addAll(courses)
+        }
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): AcademyAdapter.CourseViewHolder {
+        val itemsAcademyBinding =
+            ItemsAcademyBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return CourseViewHolder(itemsAcademyBinding)
+    }
+
+    override fun onBindViewHolder(holder: AcademyAdapter.CourseViewHolder, position: Int) {
+        val course = listCourses[position]
+        holder.bind(course)
+    }
+
+    override fun getItemCount(): Int = listCourses.size
+
+    inner class CourseViewHolder(private val binding: ItemsAcademyBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(course: CourseEntity) {
+            with(binding) {
+                tvItemTitle.text = course.title
+                tvItemDate.text =
+                    itemView.resources.getString(R.string.deadline_date, course.deadline)
+                itemView.setOnClickListener {
+                    val intent = Intent(itemView.context, DetailCourseActivity::class.java)
+                    intent.putExtra(DetailCourseActivity.EXTRA_COURSE, course.courseId)
+                    itemView.context.startActivity(intent)
+                }
+                Glide.with(itemView.context)
+                    .load(course.imagePath)
+                    .apply(RequestOptions.placeholderOf(R.drawable.ic_loading))
+                    .error(R.drawable.ic_error)
+                    .into(imgPoster)
+            }
+        }
+    }
+}
