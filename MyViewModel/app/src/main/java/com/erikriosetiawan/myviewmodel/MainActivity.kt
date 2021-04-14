ac2dbb4e28052c8b4394f7493ae808d7b9693cf2
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-04-14 11:33:41 +0700

    Integrated the view model

--- MyViewModel/app/src/main/java/com/erikriosetiawan/myviewmodel/MainActivity.kt
+++ MyViewModel/app/src/main/java/com/erikriosetiawan/myviewmodel/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.erikriosetiawan.myviewmodel
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import com.erikriosetiawan.myviewmodel.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var activityMainBinding: ActivityMainBinding
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+
+        displayResult()
+
+        activityMainBinding.btnCalculate.setOnClickListener {
+            val width = activityMainBinding.edtWidth.text.toString()
+            val height = activityMainBinding.edtHeight.text.toString()
+            val length = activityMainBinding.edtLength.text.toString()
+
+            when {
+                width.isEmpty() -> activityMainBinding.edtWidth.error =
+                    "This field not must be empty."
+                height.isEmpty() -> activityMainBinding.edtHeight.error =
+                    "This field not must be empty."
+                length.isEmpty() -> activityMainBinding.edtLength.error =
+                    "This field not must be empty."
+                else -> {
+                    viewModel.calculate(width, height, length)
+                    displayResult()
+                }
+            }
+        }
+    }
+
+    private fun displayResult() {
+        activityMainBinding.tvResult.text = viewModel.result.toString()
     }
 }
