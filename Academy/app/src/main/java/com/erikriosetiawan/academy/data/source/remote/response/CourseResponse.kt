16cf9723b8f39787a9708e374bc477af270a4105
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-04-15 13:11:13 +0700

    Create CourseResponse.kt

---
+++ Academy/app/src/main/java/com/erikriosetiawan/academy/data/source/remote/response/CourseResponse.kt
@@ -0,0 +1,9 @@
+package com.erikriosetiawan.academy.data.source.remote.response
+
+data class CourseResponse(
+    var id: String,
+    var title: String,
+    var description: String,
+    var date: String,
+    var imagePath: String
+)
