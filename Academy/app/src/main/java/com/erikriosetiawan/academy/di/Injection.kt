60fc5ff498aa514ae9d4e8606950198a491e9930
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-04-15 13:59:38 +0700

    Create Injection.kt

---
+++ Academy/app/src/main/java/com/erikriosetiawan/academy/di/Injection.kt
@@ -0,0 +1,13 @@
+package com.erikriosetiawan.academy.di
+
+import android.content.Context
+import com.erikriosetiawan.academy.data.source.AcademyRepository
+import com.erikriosetiawan.academy.data.source.remote.RemoteDataSource
+import com.erikriosetiawan.academy.utils.JsonHelper
+
+object Injection {
+    fun provideRepository(context: Context): AcademyRepository {
+        val remoteDataSource = RemoteDataSource.getInstance(JsonHelper(context))
+        return AcademyRepository.getInstance(remoteDataSource)
+    }
+}
