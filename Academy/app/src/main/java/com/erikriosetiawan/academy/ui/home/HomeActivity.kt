7548529c9c8e4107d6867e36cd13ba50277eb18b
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-04-15 09:07:48 +0700

    Create HomeActivity.kt

--- Academy/app/src/main/java/com/erikriosetiawan/academy/ui/home/HomeActivity.kt
+++ Academy/app/src/main/java/com/erikriosetiawan/academy/ui/home/HomeActivity.kt
@@ -2,11 +2,19 @@ package com.erikriosetiawan.academy.ui.home
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.erikriosetiawan.academy.R
+import com.erikriosetiawan.academy.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+
+        val activityHomeBinding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(activityHomeBinding.root)
+
+        val sectionPagerAdapter = SectionPagerAdapter(this, supportFragmentManager)
+        activityHomeBinding.viewPager.adapter = sectionPagerAdapter
+        activityHomeBinding.tabs.setupWithViewPager(activityHomeBinding.viewPager)
+
+        supportActionBar?.elevation = 0f
     }
 }
