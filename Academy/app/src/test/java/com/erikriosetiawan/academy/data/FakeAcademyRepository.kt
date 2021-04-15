04b1b31bb0060d4d385d39d9642125c788ec1833
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-04-15 18:36:53 +0700

    Create FakeAcademyRepository.kt

---
+++ Academy/app/src/test/java/com/erikriosetiawan/academy/data/FakeAcademyRepository.kt
@@ -0,0 +1,96 @@
+package com.erikriosetiawan.academy.data
+
+import com.erikriosetiawan.academy.data.source.AcademyDataSource
+import com.erikriosetiawan.academy.data.source.remote.RemoteDataSource
+
+class FakeAcademyRepository(private val remoteDataSource: RemoteDataSource) :
+    AcademyDataSource {
+
+    override fun getAllCourses(): List<CourseEntity> {
+        val courseResponses = remoteDataSource.getAllCourses()
+        val courseList = ArrayList<CourseEntity>()
+        for (response in courseResponses) {
+            val course = CourseEntity(
+                response.id,
+                response.title,
+                response.description,
+                response.date,
+                false,
+                response.imagePath
+            )
+
+            courseList.add(course)
+        }
+        return courseList
+    }
+
+    override fun getBookmarkedCourses(): List<CourseEntity> {
+        val courseResponse = remoteDataSource.getAllCourses()
+        val courseList = ArrayList<CourseEntity>()
+        for (response in courseResponse) {
+            val course = CourseEntity(
+                response.id,
+                response.title,
+                response.description,
+                response.date,
+                false,
+                response.imagePath
+            )
+            courseList.add(course)
+        }
+        return courseList
+    }
+
+    override fun getCourseWithModules(courseId: String): CourseEntity {
+        val courseResponse = remoteDataSource.getAllCourses()
+        lateinit var course: CourseEntity
+        for (response in courseResponse) {
+            if (response.id == courseId) {
+                course = CourseEntity(
+                    response.id,
+                    response.title,
+                    response.description,
+                    response.date,
+                    false,
+                    response.imagePath
+                )
+            }
+        }
+        return course
+    }
+
+    override fun getAllModulesByCourse(courseId: String): List<ModuleEntity> {
+        val moduleResponses = remoteDataSource.getModules(courseId)
+        val moduleList = ArrayList<ModuleEntity>()
+        for (response in moduleResponses) {
+            val course = ModuleEntity(
+                response.moduleId,
+                response.courseId,
+                response.title,
+                response.position,
+                false
+            )
+            moduleList.add(course)
+        }
+        return moduleList
+    }
+
+    override fun getContent(courseId: String, moduleId: String): ModuleEntity {
+        val moduleResponses = remoteDataSource.getModules(courseId)
+        lateinit var module: ModuleEntity
+        for (response in moduleResponses) {
+            if (response.moduleId == moduleId) {
+                module = ModuleEntity(
+                    response.moduleId,
+                    response.courseId,
+                    response.title,
+                    response.position,
+                    false
+                )
+                module.contentEntity = ContentEntity(remoteDataSource.getContent(moduleId).content)
+                break
+            }
+        }
+        return module
+    }
+}
