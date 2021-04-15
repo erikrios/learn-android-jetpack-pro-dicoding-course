aa2f6e6a4207283af9da6db6e67ff87a5095713c
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-04-15 08:50:27 +0700

    Create DetailCourseAdapter.kt

---
+++ Academy/app/src/main/java/com/erikriosetiawan/academy/ui/detail/DetailCourseAdapter.kt
@@ -0,0 +1,39 @@
+package com.erikriosetiawan.academy.ui.detail
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.erikriosetiawan.academy.data.ModuleEntity
+import com.erikriosetiawan.academy.databinding.ItemsModuleListBinding
+
+class DetailCourseAdapter : RecyclerView.Adapter<DetailCourseAdapter.ModuleViewHolder>() {
+
+    private val listModules = ArrayList<ModuleEntity>()
+
+    fun setModules(modules: List<ModuleEntity>?) {
+        modules?.let {
+            this.listModules.clear()
+            this.listModules.addAll(modules)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ModuleViewHolder {
+        val itemModuleListBinding =
+            ItemsModuleListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ModuleViewHolder(itemModuleListBinding)
+    }
+
+    override fun onBindViewHolder(holder: ModuleViewHolder, position: Int) {
+        val module = listModules[position]
+        holder.bind(module)
+    }
+
+    override fun getItemCount(): Int = listModules.size
+
+    inner class ModuleViewHolder(private val binding: ItemsModuleListBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(module: ModuleEntity) {
+            binding.textModuleTitle.text = module.title
+        }
+    }
+}
