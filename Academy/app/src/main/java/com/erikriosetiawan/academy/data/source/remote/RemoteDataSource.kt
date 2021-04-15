c998f16e3c60046b827d4c0ae07eefac2e2e6677
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-04-15 13:37:39 +0700

    Create RemoteDataSource.kt

---
+++ Academy/app/src/main/java/com/erikriosetiawan/academy/data/source/remote/RemoteDataSource.kt
@@ -0,0 +1,24 @@
+package com.erikriosetiawan.academy.data.source.remote
+
+import com.erikriosetiawan.academy.data.source.remote.response.ContentResponse
+import com.erikriosetiawan.academy.data.source.remote.response.CourseResponse
+import com.erikriosetiawan.academy.data.source.remote.response.ModuleResponse
+import com.erikriosetiawan.academy.utils.JsonHelper
+
+class RemoteDataSource private constructor(private val jsonHelper: JsonHelper) {
+
+    companion object {
+        @Volatile
+        private var instance: RemoteDataSource? = null
+
+        fun getInstance(helper: JsonHelper): RemoteDataSource = instance ?: synchronized(this) {
+            instance ?: RemoteDataSource(helper).apply { instance = this }
+        }
+    }
+
+    fun getAllCourses(): List<CourseResponse> = jsonHelper.loadCourses()
+
+    fun getModules(courseId: String): List<ModuleResponse> = jsonHelper.loadModule(courseId)
+
+    fun getContent(moduleId: String): ContentResponse = jsonHelper.loadContent(moduleId)
+}
