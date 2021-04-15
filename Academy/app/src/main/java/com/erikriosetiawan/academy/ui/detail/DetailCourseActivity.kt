9a63da3bd10b80052340738309374c547b187643
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-04-15 09:01:01 +0700

    Integrated the RecyclerView

--- Academy/app/src/main/java/com/erikriosetiawan/academy/ui/detail/DetailCourseActivity.kt
+++ Academy/app/src/main/java/com/erikriosetiawan/academy/ui/detail/DetailCourseActivity.kt
@@ -1,9 +1,19 @@
 package com.erikriosetiawan.academy.ui.detail
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
+import com.erikriosetiawan.academy.R
+import com.erikriosetiawan.academy.data.CourseEntity
 import com.erikriosetiawan.academy.databinding.ActivityDetailCourseBinding
 import com.erikriosetiawan.academy.databinding.ContentDetailCourseBinding
+import com.erikriosetiawan.academy.ui.reader.CourseReaderActivity
+import com.erikriosetiawan.academy.utils.DataDummy
 
 class DetailCourseActivity : AppCompatActivity() {
 
@@ -23,5 +33,51 @@ class DetailCourseActivity : AppCompatActivity() {
 
         setSupportActionBar(activityDetailCourseBinding.toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val adapter = DetailCourseAdapter()
+
+        val extras = intent.extras
+        extras?.let {
+            val courseId = extras.getString(EXTRA_COURSE)
+            courseId?.let {
+                val modules = DataDummy.generateDummyModules(courseId)
+                adapter.setModules(modules)
+                for (course in DataDummy.generateDummyCourses()) {
+                    populateCourse(course)
+                }
+            }
+        }
+
+        with(detailContentBinding.rvModule) {
+            isNestedScrollingEnabled = false
+            layoutManager = LinearLayoutManager(this@DetailCourseActivity)
+            setHasFixedSize(true)
+            this.adapter = adapter
+            val dividerItemDecoration =
+                DividerItemDecoration(this.context, DividerItemDecoration.VERTICAL)
+            addItemDecoration(dividerItemDecoration)
+        }
+    }
+
+    private fun populateCourse(courseEntity: CourseEntity) {
+        detailContentBinding.textTitle.text = courseEntity.title
+        detailContentBinding.textDescription.text = courseEntity.description
+        detailContentBinding.textDate.text =
+            resources.getString(R.string.deadline_date, courseEntity.deadline)
+
+        Glide.with(this)
+            .load(courseEntity.imagePath)
+            .transform(RoundedCorners(20))
+            .apply(
+                RequestOptions.placeholderOf(R.drawable.ic_loading)
+                    .error(R.drawable.ic_error)
+            )
+            .into(detailContentBinding.imagePoster)
+
+        detailContentBinding.btnStart.setOnClickListener {
+            val intent = Intent(this@DetailCourseActivity, CourseReaderActivity::class.java)
+            intent.putExtra(CourseReaderActivity.EXTRA_COURSE_ID, courseEntity.courseId)
+            startActivity(intent)
+        }
     }
 }
