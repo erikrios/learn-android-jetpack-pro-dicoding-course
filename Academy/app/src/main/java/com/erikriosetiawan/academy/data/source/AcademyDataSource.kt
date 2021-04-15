29d05ef51580b0e394df758cb183b7c657e81d91
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-04-15 13:39:53 +0700

    Create AcademyDataSource.kt

---
+++ Academy/app/src/main/java/com/erikriosetiawan/academy/data/source/AcademyDataSource.kt
@@ -0,0 +1,17 @@
+package com.erikriosetiawan.academy.data.source
+
+import com.erikriosetiawan.academy.data.CourseEntity
+import com.erikriosetiawan.academy.data.ModuleEntity
+
+interface AcademyDataSource {
+
+    fun getAllCourses(): List<CourseEntity>
+
+    fun getBookmarkedCourses(): List<CourseEntity>
+
+    fun getCourseWithModules(courseId: String): CourseEntity
+
+    fun getAllModulesByCourse(courseId: String): List<ModuleEntity>
+
+    fun getContent(courseId: String, moduleId: String): ModuleEntity
+}
