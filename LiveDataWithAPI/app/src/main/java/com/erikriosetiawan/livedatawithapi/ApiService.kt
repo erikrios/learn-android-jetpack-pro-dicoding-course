0ab4f12f39ebba673df4c07a52bd8fa81a2af2ea
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-04-16 13:49:39 +0700

    Create ApiService.kt

---
+++ LiveDataWithAPI/app/src/main/java/com/erikriosetiawan/livedatawithapi/ApiService.kt
@@ -0,0 +1,13 @@
+package com.erikriosetiawan.livedatawithapi
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface ApiService {
+
+    @GET("detail/{id}")
+    fun getRestaurant(
+        @Path("id") id: String
+    ): Call<RestaurantResponse>
+}
