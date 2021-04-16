fbdd810964f9895c3f45d6d7e7c8d7d82ddb7e1f
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-04-16 21:23:49 +0700

    Implements the espresso idling resource

--- MyIdlingResource/app/src/androidTest/java/com/erikriosetiawan/myidlingresource/MainActivityInstrumentedTest.kt
+++ MyIdlingResource/app/src/androidTest/java/com/erikriosetiawan/myidlingresource/MainActivityInstrumentedTest.kt
@@ -3,12 +3,14 @@ package com.erikriosetiawan.myidlingresource
 import android.content.Context
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.IdlingRegistry
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -23,6 +25,13 @@ class MainActivityInstrumentedTest {
         instrumentalContext = InstrumentationRegistry.getInstrumentation().targetContext
 
         ActivityScenario.launch(MainActivity::class.java)
+
+        IdlingRegistry.getInstance().register(EspressoIdlingResource.getEspressoIdlingResource())
+    }
+
+    @After
+    fun tearDown() {
+        IdlingRegistry.getInstance().unregister(EspressoIdlingResource.getEspressoIdlingResource())
     }
 
     @Test
