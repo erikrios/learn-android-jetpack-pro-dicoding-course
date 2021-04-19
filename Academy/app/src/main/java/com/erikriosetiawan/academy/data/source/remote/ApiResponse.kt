56ab3ce8e967045f5495354a6d0f376c01234d55
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-04-19 22:42:07 +0700

    Refactor the code

--- Academy/app/src/main/java/com/erikriosetiawan/academy/data/source/remote/ApiResponse.kt
+++ Academy/app/src/main/java/com/erikriosetiawan/academy/data/source/remote/ApiResponse.kt
@@ -1,6 +1,6 @@
 package com.erikriosetiawan.academy.data.source.remote
 
-class ApiResponse<T>(val status: StatusResponse, val body: T, message: String?) {
+class ApiResponse<T>(val status: StatusResponse, val body: T, val message: String?) {
 
     companion object {
         fun <T> success(body: T): ApiResponse<T> = ApiResponse(StatusResponse.SUCCESS, body, null)
