be07b8342dea2211fafe9281e2534c4618c1d7c7
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-04-19 20:34:01 +0700

    Create NoteAdapter.kt

---
+++ MyNoteApps/app/src/main/java/com/erikriosetiawan/mynoteapps/ui/main/NoteAdapter.kt
@@ -0,0 +1,54 @@
+package com.erikriosetiawan.mynoteapps.ui.main
+
+import android.app.Activity
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.erikriosetiawan.mynoteapps.database.Note
+import com.erikriosetiawan.mynoteapps.databinding.ItemNoteBinding
+import com.erikriosetiawan.mynoteapps.helper.NoteDiffCallback
+import com.erikriosetiawan.mynoteapps.ui.insert.NoteAddUpdateActivity
+
+class NoteAdapter internal constructor(private val activity: Activity) :
+    RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
+
+    private val listNotes = ArrayList<Note>()
+
+    fun setListNotes(listNotes: List<Note>) {
+        val diffCallback = NoteDiffCallback(this.listNotes, listNotes)
+        val diffResult = DiffUtil.calculateDiff(diffCallback)
+
+        this.listNotes.clear()
+        this.listNotes.addAll(listNotes)
+        diffResult.dispatchUpdatesTo(this)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        val binding = ItemNoteBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return NoteViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) =
+        holder.bind(listNotes[position], position)
+
+    override fun getItemCount(): Int = listNotes.size
+
+    inner class NoteViewHolder(private val binding: ItemNoteBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(note: Note, position: Int) {
+            with(binding) {
+                tvItemTitle.text = note.title
+                tvItemDate.text = note.date
+                tvItemDescription.text = note.description
+                cvItemNote.setOnClickListener {
+                    val intent = Intent(activity, NoteAddUpdateActivity::class.java)
+                    intent.putExtra(NoteAddUpdateActivity.EXTRA_POSITION, position)
+                    intent.putExtra(NoteAddUpdateActivity.EXTRA_NOTE, note)
+                    activity.startActivityForResult(intent, NoteAddUpdateActivity.REQUEST_UPDATE)
+                }
+            }
+        }
+    }
+}
