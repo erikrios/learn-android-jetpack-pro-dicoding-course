f7a4d6c98f5e1119af63b4debb5b9e7527aac6c7
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-04-19 19:38:09 +0700

    Create NoteAddUpdateActivity.kt

---
+++ MyNoteApps/app/src/main/java/com/erikriosetiawan/mynoteapps/ui/insert/NoteAddUpdateActivity.kt
@@ -0,0 +1,12 @@
+package com.erikriosetiawan.mynoteapps.ui.insert
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.erikriosetiawan.mynoteapps.R
+
+class NoteAddUpdateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_note_add_update)
+    }
+}
