8b79ef67148707445f68adb35a34db00e6e0113d
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-04-19 20:43:50 +0700

    Integrated the view model

--- MyNoteApps/app/src/main/java/com/erikriosetiawan/mynoteapps/ui/main/MainActivity.kt
+++ MyNoteApps/app/src/main/java/com/erikriosetiawan/mynoteapps/ui/main/MainActivity.kt
@@ -1,12 +1,60 @@
 package com.erikriosetiawan.mynoteapps.ui.main
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.erikriosetiawan.mynoteapps.R
+import com.erikriosetiawan.mynoteapps.database.Note
+import com.erikriosetiawan.mynoteapps.databinding.ActivityMainBinding
+import com.erikriosetiawan.mynoteapps.helper.ViewModelFactory
+import com.erikriosetiawan.mynoteapps.ui.insert.NoteAddUpdateActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private var _activityMainBinding: ActivityMainBinding? = null
+    private val binding get() = _activityMainBinding
+
+    private lateinit var adapter: NoteAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        _activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        val mainViewModel = obtainViewModel(this@MainActivity)
+        mainViewModel.getAllNotes().observe(this, noteObserver)
+
+        adapter = NoteAdapter(this@MainActivity)
+
+        binding?.rvNotes?.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            setHasFixedSize(true)
+            adapter = adapter
+        }
+
+        binding?.fabAdd?.setOnClickListener { view ->
+            if (view.id == R.id.fab_add) {
+                val intent = Intent(this@MainActivity, NoteAddUpdateActivity::class.java)
+                startActivityForResult(intent, NoteAddUpdateActivity.REQUEST_ADD)
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _activityMainBinding = null
+    }
+
+    private fun obtainViewModel(activity: AppCompatActivity): MainViewModel {
+        val factory = ViewModelFactory.getInstance(activity.application)
+        return ViewModelProvider(activity, factory).get(MainViewModel::class.java)
+    }
+
+    private val noteObserver = Observer<List<Note>> { noteList ->
+        noteList?.let { adapter.setListNotes(noteList) }
     }
 }
