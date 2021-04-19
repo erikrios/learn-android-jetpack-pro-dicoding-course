24f4370b701ff3e3233e344373c91ebd8f3be20c
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-04-19 22:50:11 +0700

    Refactor the code

--- Academy/app/src/main/java/com/erikriosetiawan/academy/di/Injection.kt
+++ Academy/app/src/main/java/com/erikriosetiawan/academy/di/Injection.kt
@@ -2,12 +2,21 @@ package com.erikriosetiawan.academy.di
 
 import android.content.Context
 import com.erikriosetiawan.academy.data.AcademyRepository
+import com.erikriosetiawan.academy.data.source.local.LocalDataSource
+import com.erikriosetiawan.academy.data.source.local.room.AcademyDatabase
 import com.erikriosetiawan.academy.data.source.remote.RemoteDataSource
+import com.erikriosetiawan.academy.utils.AppExecutors
 import com.erikriosetiawan.academy.utils.JsonHelper
 
 object Injection {
     fun provideRepository(context: Context): AcademyRepository {
+
+        val database = AcademyDatabase.getInstance(context)
+
+
         val remoteDataSource = RemoteDataSource.getInstance(JsonHelper(context))
-        return AcademyRepository.getInstance(remoteDataSource)
+        val localDataSource = LocalDataSource.getInstance(database.academyDao())
+        val appExecutors = AppExecutors()
+        return AcademyRepository.getInstance(remoteDataSource, localDataSource, appExecutors)
     }
 }
