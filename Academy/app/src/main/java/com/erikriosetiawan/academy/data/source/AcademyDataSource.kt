b462d7e694965191f5e98a91978b264a13711a7f
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-04-19 22:46:56 +0700

    Add some methods

--- Academy/app/src/main/java/com/erikriosetiawan/academy/data/source/AcademyDataSource.kt
+++ Academy/app/src/main/java/com/erikriosetiawan/academy/data/source/AcademyDataSource.kt
@@ -2,17 +2,23 @@ package com.erikriosetiawan.academy.data.source
 
 import androidx.lifecycle.LiveData
 import com.erikriosetiawan.academy.data.source.local.entity.CourseEntity
+import com.erikriosetiawan.academy.data.source.local.entity.CourseWithModule
 import com.erikriosetiawan.academy.data.source.local.entity.ModuleEntity
+import com.erikriosetiawan.academy.vo.Resource
 
 interface AcademyDataSource {
 
-    fun getAllCourses(): LiveData<List<CourseEntity>>
+    fun getAllCourses(): LiveData<Resource<List<CourseEntity>>>
 
     fun getBookmarkedCourses(): LiveData<List<CourseEntity>>
 
-    fun getCourseWithModules(courseId: String): LiveData<CourseEntity>
+    fun getCourseWithModules(courseId: String): LiveData<Resource<CourseWithModule>>
 
-    fun getAllModulesByCourse(courseId: String): LiveData<List<ModuleEntity>>
+    fun getAllModulesByCourse(courseId: String): LiveData<Resource<List<ModuleEntity>>>
 
-    fun getContent(courseId: String, moduleId: String): LiveData<ModuleEntity>
+    fun getContent(moduleId: String): LiveData<Resource<ModuleEntity>>
+
+    fun setCourseBookmark(course: CourseEntity, state: Boolean)
+
+    fun setReadModule(module: ModuleEntity)
 }
