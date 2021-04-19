b5a063bc9c4fd810d98b0131af9e5c8dad476d73
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-04-19 19:22:45 +0700

    Create NoteRepository.kt

---
+++ MyNoteApps/app/src/main/java/com/erikriosetiawan/mynoteapps/repository/NoteRepository.kt
@@ -0,0 +1,33 @@
+package com.erikriosetiawan.mynoteapps.repository
+
+import android.app.Application
+import androidx.lifecycle.LiveData
+import com.erikriosetiawan.mynoteapps.database.Note
+import com.erikriosetiawan.mynoteapps.database.NoteDao
+import com.erikriosetiawan.mynoteapps.database.NoteRoomDatabase
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+class NoteRepository(application: Application) {
+    private val mNotesDao: NoteDao
+    private val executorService: ExecutorService = Executors.newSingleThreadExecutor()
+
+    init {
+        val db = NoteRoomDatabase.getDatabase(application)
+        mNotesDao = db.noteDao()
+    }
+
+    fun getAllNotes(): LiveData<List<Note>> = mNotesDao.getAllNotes()
+
+    fun insert(note: Note) {
+        executorService.execute { mNotesDao.insert(note) }
+    }
+
+    fun delete(note: Note) {
+        executorService.execute { mNotesDao.delete(note) }
+    }
+
+    fun update(note: Note) {
+        executorService.execute { mNotesDao.update(note) }
+    }
+}
