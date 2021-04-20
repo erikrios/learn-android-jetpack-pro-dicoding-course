cf43c2a4d67ca383e2bd4499233b78a025b321cf
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-04-20 13:24:46 +0700

    Add a method to insert dummy notes

--- MyNoteApps/app/src/main/java/com/erikriosetiawan/mynoteapps/database/NoteRoomDatabase.kt
+++ MyNoteApps/app/src/main/java/com/erikriosetiawan/mynoteapps/database/NoteRoomDatabase.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import java.util.concurrent.Executors
 
 @Database(entities = [Note::class], version = 1)
 abstract class NoteRoomDatabase : RoomDatabase() {
@@ -22,10 +24,31 @@ abstract class NoteRoomDatabase : RoomDatabase() {
                         context.applicationContext,
                         NoteRoomDatabase::class.java,
                         "note_database"
-                    ).build()
+                    ).addCallback(object : Callback() {
+                        override fun onCreate(db: SupportSQLiteDatabase) {
+                            add()
+                        }
+                    })
+                        .build()
                 }
             }
             return INSTANCE as NoteRoomDatabase
+        }
+
+        fun add() {
+            Executors.newSingleThreadExecutor().execute {
+                val list: MutableList<Note> = ArrayList()
+                for (i in 0..9) {
+                    val dummyNote = Note()
+                    dummyNote.apply {
+                        title = "Tugas $i"
+                        description = "Belajar Modul $i"
+                        date = "2019/09/09 09:09:0$i"
+                    }
+                    list.add(dummyNote)
+                }
+                INSTANCE?.noteDao()?.insertAll(list)
+            }
         }
     }
 }
