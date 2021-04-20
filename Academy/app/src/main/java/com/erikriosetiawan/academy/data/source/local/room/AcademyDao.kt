1cb9da44ea8bbede1f6055fa28aa8f2dc0c5036e
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-04-20 13:38:47 +0700

    Change the return value

--- Academy/app/src/main/java/com/erikriosetiawan/academy/data/source/local/room/AcademyDao.kt
+++ Academy/app/src/main/java/com/erikriosetiawan/academy/data/source/local/room/AcademyDao.kt
@@ -1,6 +1,7 @@
 package com.erikriosetiawan.academy.data.source.local.room
 
 import androidx.lifecycle.LiveData
+import androidx.paging.DataSource
 import androidx.room.*
 import com.erikriosetiawan.academy.data.source.local.entity.CourseEntity
 import com.erikriosetiawan.academy.data.source.local.entity.CourseWithModule
@@ -10,10 +11,10 @@ import com.erikriosetiawan.academy.data.source.local.entity.ModuleEntity
 interface AcademyDao {
 
     @Query("SELECT * FROM courseentities")
-    fun getCourses(): LiveData<List<CourseEntity>>
+    fun getCourses(): DataSource.Factory<Int, CourseEntity>
 
     @Query("SELECT * FROM courseentities WHERE bookmarked = 1")
-    fun getBookmarkedCourse(): LiveData<List<CourseEntity>>
+    fun getBookmarkedCourse(): DataSource.Factory<Int, CourseEntity>
 
     @Transaction
     @Query("SELECT * FROM courseentities WHERE courseId = :courseId")
