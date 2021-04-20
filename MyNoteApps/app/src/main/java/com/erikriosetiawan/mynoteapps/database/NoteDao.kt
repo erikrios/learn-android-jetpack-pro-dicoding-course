aac5b5ba863f65151b7b549a0077a60056bd6aec
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-04-20 14:54:10 +0700

    Implements RawQuery

--- MyNoteApps/app/src/main/java/com/erikriosetiawan/mynoteapps/database/NoteDao.kt
+++ MyNoteApps/app/src/main/java/com/erikriosetiawan/mynoteapps/database/NoteDao.kt
@@ -2,6 +2,7 @@ package com.erikriosetiawan.mynoteapps.database
 
 import androidx.paging.DataSource
 import androidx.room.*
+import androidx.sqlite.db.SupportSQLiteQuery
 
 @Dao
 interface NoteDao {
@@ -14,8 +15,8 @@ interface NoteDao {
     @Delete
     fun delete(note: Note)
 
-    @Query("SELECT * FROM note ORDER BY id ASC")
-    fun getAllNotes(): DataSource.Factory<Int, Note>
+    @RawQuery(observedEntities = [Note::class])
+    fun getAllNotes(query: SupportSQLiteQuery): DataSource.Factory<Int, Note>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     fun insertAll(list: List<Note>)
