3531d5faff8cbf75aebb6f39e09c27af728c4384
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-04-20 13:45:52 +0700

    Implements paging

--- Academy/app/src/main/java/com/erikriosetiawan/academy/ui/academy/AcademyViewModel.kt
+++ Academy/app/src/main/java/com/erikriosetiawan/academy/ui/academy/AcademyViewModel.kt
@@ -2,10 +2,12 @@ package com.erikriosetiawan.academy.ui.academy
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.paging.PagedList
 import com.erikriosetiawan.academy.data.AcademyRepository
 import com.erikriosetiawan.academy.data.source.local.entity.CourseEntity
 import com.erikriosetiawan.academy.vo.Resource
 
 class AcademyViewModel(private val academyRepository: AcademyRepository) : ViewModel() {
-    fun getCourses(): LiveData<Resource<List<CourseEntity>>> = academyRepository.getAllCourses()
+    fun getCourses(): LiveData<Resource<PagedList<CourseEntity>>> =
+        academyRepository.getAllCourses()
 }
--- Academy/app/src/main/java/com/erikriosetiawan/academy/ui/bookmark/BookmarkViewModel.kt
+++ Academy/app/src/main/java/com/erikriosetiawan/academy/ui/bookmark/BookmarkViewModel.kt
@@ -2,10 +2,11 @@ package com.erikriosetiawan.academy.ui.bookmark
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.paging.PagedList
 import com.erikriosetiawan.academy.data.AcademyRepository
 import com.erikriosetiawan.academy.data.source.local.entity.CourseEntity
 
 class BookmarkViewModel(private val academyRepository: AcademyRepository) : ViewModel() {
 
-    fun getBookmarks(): LiveData<List<CourseEntity>> = academyRepository.getBookmarkedCourses()
+    fun getBookmarks(): LiveData<PagedList<CourseEntity>> = academyRepository.getBookmarkedCourses()
 }
