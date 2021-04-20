772cf7188510e1c327be503ab2ecb563945aa79c
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-04-20 15:04:56 +0700

    Implements RawQuery

--- MyNoteApps/app/src/main/java/com/erikriosetiawan/mynoteapps/ui/main/MainActivity.kt
+++ MyNoteApps/app/src/main/java/com/erikriosetiawan/mynoteapps/ui/main/MainActivity.kt
@@ -69,7 +69,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_form, menu)
+        menuInflater.inflate(R.menu.menu_main, menu)
         return super.onCreateOptionsMenu(menu)
     }
 
