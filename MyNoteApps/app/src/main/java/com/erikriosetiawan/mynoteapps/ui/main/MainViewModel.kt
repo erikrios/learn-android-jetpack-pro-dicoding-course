a9ad3db1d22069108f8821a4b65bfe160f1f4238
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-04-20 13:07:07 +0700

    Change the return value

--- MyNoteApps/app/src/main/java/com/erikriosetiawan/mynoteapps/ui/main/MainViewModel.kt
+++ MyNoteApps/app/src/main/java/com/erikriosetiawan/mynoteapps/ui/main/MainViewModel.kt
@@ -3,6 +3,8 @@ package com.erikriosetiawan.mynoteapps.ui.main
 import android.app.Application
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.paging.LivePagedListBuilder
+import androidx.paging.PagedList
 import com.erikriosetiawan.mynoteapps.database.Note
 import com.erikriosetiawan.mynoteapps.repository.NoteRepository
 
@@ -10,5 +12,6 @@ class MainViewModel(application: Application) : ViewModel() {
 
     private val mNoteRepository: NoteRepository = NoteRepository(application)
 
-    fun getAllNotes(): LiveData<List<Note>> = mNoteRepository.getAllNotes()
+    fun getAllNotes(): LiveData<PagedList<Note>> =
+        LivePagedListBuilder(mNoteRepository.getAllNotes(), 20).build()
 }
