181f6a9970beeea3bc9254c476ecfa7a8c7ffb34
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-04-20 12:57:39 +0700

    Change the return value

--- MyNoteApps/app/src/main/java/com/erikriosetiawan/mynoteapps/repository/NoteRepository.kt
+++ MyNoteApps/app/src/main/java/com/erikriosetiawan/mynoteapps/repository/NoteRepository.kt
@@ -1,7 +1,7 @@
 package com.erikriosetiawan.mynoteapps.repository
 
 import android.app.Application
-import androidx.lifecycle.LiveData
+import androidx.paging.DataSource
 import com.erikriosetiawan.mynoteapps.database.Note
 import com.erikriosetiawan.mynoteapps.database.NoteDao
 import com.erikriosetiawan.mynoteapps.database.NoteRoomDatabase
@@ -17,7 +17,7 @@ class NoteRepository(application: Application) {
         mNotesDao = db.noteDao()
     }
 
-    fun getAllNotes(): LiveData<List<Note>> = mNotesDao.getAllNotes()
+    fun getAllNotes(): DataSource.Factory<Int, Note> = mNotesDao.getAllNotes()
 
     fun insert(note: Note) {
         executorService.execute { mNotesDao.insert(note) }
